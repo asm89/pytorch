0cba7c6bc2da88f02170458211bd32ad494a7e5e
angelayi <yiangela7@gmail.com>
2023-10-24 09:20:21 -0700

    [aotinductor] Pass TorchIR to AOTInductor (#110020)
    Summary:
    Updates `_export.aot_compile` to pass a torch IR graph to inductor, allowing inductor to now run the pre_grad_passes, and reuse more of inductor's code.
    Also updates the API to only return the `so_path`, and not returning the exported program. The pytree call spec is now serialized and placed inside of the generated model code. When calling the model, because there is no c++ pytree implementation linked yet, we can access the call specs through `get_call_spec()`, and call pytree flatten/unflattenin python.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler avikchaudhuri gmagogsfm zhxchen17 tugsbayasgalan
    
    
    Test Plan: Internal testing results: https://www.internalfb.com/diff/D49599792?dst_version_fbid=873731594363746&transaction_fbid=659025106213571
    
    Reviewed By: frank-wei, desertfire
    
    Differential Revision: D49599792
    
    Pulled By: angelayi

--- torch/csrc/inductor/aoti_runtime/model.h
+++ torch/csrc/inductor/aoti_runtime/model.h
@@ -247,6 +247,14 @@ class AOTInductorModelBase {
     return constants_info_.at(idx).data_size;
   }
 
+  const char* get_in_spec() const {
+    return in_spec_.c_str();
+  }
+
+  const char* get_out_spec() const {
+    return out_spec_.c_str();
+  }
+
   void update_constants_map(std::shared_ptr<ConstantMap>&& constants_map) {
     constants_ = std::move(constants_map);
   }
@@ -301,6 +309,8 @@ class AOTInductorModelBase {
   std::vector<ParamInfo> inputs_info_;
   std::vector<ParamInfo> outputs_info_;
   std::vector<ConstInfo> constants_info_;
+  std::string in_spec_;
+  std::string out_spec_;
 
   std::shared_ptr<ConstantMap> constants_;
 
